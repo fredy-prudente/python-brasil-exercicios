e4046cc19cff945100cb935064cfe80484ff9fd4
Fredy Prudente <thenomefredy@hotmail.com>
2021-09-03 18:54:26 -0300

    Questão 17

--- Exercicios Listas/ex17.py
+++ Exercicios Listas/ex17.py
@@ -1,17 +1,39 @@
 """
-Em uma competição de salto em distância cada atleta tem direito a cinco saltos. O resultado do atleta será determinado pela média dos cinco valores restantes. Você deve fazer um programa que receba o nome e as cinco distâncias alcançadas pelo atleta em seus saltos e depois informe o nome, os saltos e a média dos saltos. O programa deve ser encerrado quando não for informado o nome do atleta.
+Em uma competição de salto em distância cada atleta tem direito a cinco saltos. 
+O resultado do atleta será determinado pela média dos cinco valores restantes. 
+Você deve fazer um programa que receba o nome e as cinco distâncias alcançadas 
+ser encerrado quando não for informado o nome do atleta. A saída do programa 
+deve ser conforme o exemplo abaixo: 
 """
 
-nome = '-1'
+nome = '0'
+nomes = []
+primeirosaltos = []
+segundosaltos = []
+terceirosaltos = []
+quartosaltos = []
+quintosaltos = []
+medias = []
 while nome != '-1':
     nome = input('Digite o nome do Atleta: ')
     if nome != '-1':
-        primeiro_salto = float(input('Primeiro Salto: '))
-        segundo_salto = float(input('Segundo Salto: '))
-        terceiro_salto = float(input('Terceiro Salto: '))
-        quarto_salto = float(input('Quarto Salto: '))
-        quinto_salto = float(input('Quinto Salto: '))
-        media = (primeiro_salto + segundo_salto + terceiro_salto + quarto_salto + quinto_salto) / 5
-        print(f'Atleta: {nome}\n'
-        f'\n'
-        f'Primeiro Salto: {primeiro_salto}')
+        nomes.append(nome)
+        primeirosalto = float(input('Primeiro Salto: '))
+        primeirosaltos.append(primeirosalto)
+        segundosalto = float(input('Segundo Salto: '))
+        segundosaltos.append(segundosalto)
+        terceirosalto = float(input('Terceiro Salto: '))
+        terceirosaltos.append(terceirosalto)
+        quartosalto = float(input('Quarto Salto: '))
+        quartosaltos.append(quartosalto)
+        quintosalto = float(input('Quinto Salto: '))
+        quintosaltos.append(quintosalto)
+        media = (primeirosalto + segundosalto +
+                 terceirosalto + quartosalto + quintosalto) / 5
+        medias.append(media)
+print(f'Resultado Final:\n')
+for i in range(len(nomes)):
+    print(f'Atleta: {nomes[i]}\n'
+          f'Saltos: {primeirosaltos[i]} - {segundosaltos[i]} - {terceirosaltos[i]} - {quartosaltos[i]} - '
+          f'{quintosaltos[i]}\n'
+          f'Média dos saltos: {medias[i]:.2f} m')
